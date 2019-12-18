86190ffc714f609fb2c0714d6c852e98a6837af6
ujikstark <57621743+ujikstark@users.noreply.github.com>
2019-12-19 06:22:56 +0700

    Update ujikscrap.py

--- reversewhois/ujikscrap.py
+++ reversewhois/ujikscrap.py
@@ -1,1 +1,20 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as bs4
+
+
+site = input("Enter Domain Name : ")
+url = ('https://viewdns.info/reversewhois/?q=' + site)
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+html = urlopen(req).read()
+webpage = html.decode('utf-8')
+soup = bs4(webpage, "html.parser")
+
+
+result = soup.find_all("table", attrs={"border" : "1"})
+
+for i in result:
+	row = ''
+	rows = i.findAll('tr')
+	for row in rows:
+		print(row.getText(' '))
 
