b80c9540edec96e151460d7060836abd8c28e78e
ujikstark <57621743+ujikstark@users.noreply.github.com>
2020-01-18 09:14:10 -0500

    Update hashing.py

--- hashingandcracking/hashing.py
+++ hashingandcracking/hashing.py
@@ -1,3 +1,9 @@
+## Created By Ujikstark
+
+#!/usr/bin/env python3
+import hashlib
+from termcolor import colored
+
 import hashlib
 from termcolor import colored
 
@@ -6,7 +12,7 @@ def hash_md5():
 	encode_md5 = hashlib.md5(str(word_hash).encode('utf-8'))
 	result = encode_md5.hexdigest() 
 	a = f'{result} = {word_hash}\n'
-	print(colored(f'\n{a}', "green"))
+	print(colored(f'\n{word_hash} = {result}\n', "green"))
 	
 	with open("ujik.txt", 'a') as f:
 		f.write(a) 
@@ -16,7 +22,7 @@ def hash_sha1():
 	encode_sha1 = hashlib.sha1(str(word_hash).encode('utf-8'))
 	result = encode_sha1.hexdigest() 
 	a = f'{result} = {word_hash}\n'
-	print(colored(f'\n{a}', "green"))
+	print(colored(f'\n{word_hash} = {result}\n', "green"))
 	
 	with open("ujik.txt", 'a') as f:
 		f.write(a) 
@@ -26,7 +32,7 @@ def hash_sha224():
 	encode_sha224 = hashlib.sha224(str(word_hash).encode('utf-8'))
 	result = encode_sha224.hexdigest() 
 	a = f'{result} = {word_hash}\n'
-	print(colored(f'\n{a}', "green"))
+	print(colored(f'\n{word_hash} = {result}\n', "green"))
 	
 	with open("ujik.txt", 'a') as f:
 		f.write(a) 
@@ -36,7 +42,7 @@ def hash_sha256():
 	encode_sha256 = hashlib.sha256(str(word_hash).encode('utf-8'))
 	result = encode_sha256.hexdigest() 
 	a = f'{result} = {word_hash}\n'
-	print(colored(f'\n{a}', "green"))
+	print(colored(f'\n{word_hash} = {result}\n', "green"))
 	
 	with open("ujik.txt", 'a') as f:
 		f.write(a) 
@@ -47,7 +53,7 @@ def hash_sha384():
 	encode_sha384 = hashlib.sha384(str(word_hash).encode('utf-8'))
 	result = encode_sha384.hexdigest() 
 	a = f'{result} = {word_hash}\n'
-	print(colored(f'\n{a}', "green"))
+	print(colored(f'\n{word_hash} = {result}\n', "green"))
 	
 	with open("ujik.txt", 'a') as f:
 		f.write(a) 
@@ -57,7 +63,8 @@ def hash_sha512():
 	sha512 = hashlib.sha512()
 	encode_sha512 = hashlib.sha512(str(word_hash).encode('utf-8'))
 	a = f'{result} = {word_hash}\n'
-	print(colored(f'\n{a}', "green"))
+	print(colored(f'\n{word_hash} = {result}\n', "green"))
 	
 	with open("ujik.txt", 'a') as f:
 		f.write(a) 
+
